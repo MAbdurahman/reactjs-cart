48ded51b1dcf55ba49c5f8758898abce40fcfc02
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-24 08:53:28 -0500

    All functionality appear to work

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useGlobalContext } from './../utils/context';
 
 export default function CartItem({ id, img, title, price, amount }) {
-   
+
 	const { remove, increase, decrease, toggleAmount } = useGlobalContext();
 
 	return (
@@ -20,7 +20,7 @@ export default function CartItem({ id, img, title, price, amount }) {
 				{/* increase amount */}
 				<button
 					className='amount-btn'
-					onClick={() => toggleAmount(id, 'inc')}
+					onClick={() => increase(id)}
 				>
 					<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
 						<path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
@@ -31,7 +31,7 @@ export default function CartItem({ id, img, title, price, amount }) {
 				{/* decrease amount */}
 				<button
 					className='amount-btn'
-					onClick={() => toggleAmount(id, 'dec')}
+					onClick={() => decrease(id)}
 				>
 					<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
 						<path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
