4ec849d9186da27681c783c59c4ff3599f373b20
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-24 05:11:02 -0500

    Modify: CartContainer.jsx, Navbar.jsx, App.jsx; Add reducer.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { SpinningCircles } from 'react-loading-icons';
+import Navbar from './../components/Navbar';
+import CartContainer from './../components/CartContainer';
+import { useGlobalContext } from './../utils/context';
 
 export default function App() {
-  return (
-    <div>
-      <h2>This is App</h2>
-    </div>
-  )
+	const { loading } = useGlobalContext();
+	if (loading) {
+		return (
+			<div className='loading'>
+				<SpinningCircles
+					height='8em'
+					width='8em'
+					fill='#003e6b'
+					stroke='#003e6b'
+					speed={1}
+					fillOpacity={1}
+					strokeWidth={2}
+					strokeOpacity={0.725}
+				/>
+        <h4>Loading . . .</h4>
+			</div>
+		);
+	}
+	return (
+		<main>
+			<Navbar />
+			<CartContainer />
+		</main>
+	);
 }
