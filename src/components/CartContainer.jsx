c6d95d7cdbd56f8f025e200a4ccf534b4d87917c
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-24 07:58:20 -0500

    Modify: update styles on .cart img

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import CartItem from './CartItem';
+import EmptyCart from './../img/empty-cart.png'
 import { useGlobalContext } from './../utils/context';
 
 export default function CartContainer() {
 	const { cart, total, clearCart } = useGlobalContext();
-   
+
 	if (cart.length === 0) {
 		return (
 			<section className='cart'>
 				{/* cart header */}
 				<header>
-					<h2>your bag</h2>
-					<h4 className='empty-cart'>is currently empty</h4>
+					<h2>your cart</h2>
+               <img src={EmptyCart} alt='empty cart' />
+					<h3 className='empty-cart'>Is Empty!</h3>
 				</header>
 			</section>
 		);
