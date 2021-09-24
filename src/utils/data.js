2d531cfa04138abeab975fc1c8ec461f5acf75f7
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-24 04:13:02 -0500

    Add data.js

---
+++ src/utils/data.js
@@ -0,0 +1,37 @@
+export default [
+   {
+      id: 1,
+      title: 'Sony PlayStation',
+      price: 499.99,
+      img: 'https://res.cloudinary.com/mdbdrrhm/image/upload/v1632473882/products/playstation_ofgm9u.jpg',
+      amount: 1,
+   },
+	{
+		id: 2,
+		title: 'Samsung Galaxy S7',
+		price: 599.99,
+		img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png',
+		amount: 1,
+	},
+	{
+		id: 3,
+		title: 'Google Pixel ',
+		price: 499.99,
+		img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png',
+		amount: 1,
+	},
+	{
+		id: 4,
+		title: 'Xiaomi Redmi Note 2',
+		price: 699.99,
+		img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png',
+		amount: 1,
+	},
+	{
+		id: 5,
+		title: 'Apple Airpods',
+		price: 129.99,
+		img: 'https://res.cloudinary.com/mdbdrrhm/image/upload/v1632473882/products/airpods_xtwq6b.jpg',
+		amount: 1,
+	},
+];
